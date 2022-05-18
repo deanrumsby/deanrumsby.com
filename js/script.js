99388a380e7052d1d0018d628d70a3309d7f74b4
Dean Rumsby <dean@deanrumsby.com>
2022-05-18 12:29:42 +0100

    add hover opacity change to tech grid items and portfolio items

--- js/script.js
+++ js/script.js
@@ -3,7 +3,10 @@ const fixTechGridHeight = () => {
   const techGrid = document.querySelector('.tech-list');
   const compStyles = window.getComputedStyle(profile);
   const profileHeight = compStyles.getPropertyValue('height');
+  console.log(profileHeight);
   techGrid.style.maxHeight = profileHeight;
 }
+
+window.addEventListener('resize', fixTechGridHeight);
 
 fixTechGridHeight();
