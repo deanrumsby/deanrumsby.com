d58160fa7373b814d887ba407bce9a8988e56aa9
Dean Rumsby <dean@deanrumsby.com>
2022-05-18 18:23:44 +0100

    add captions to each of the tech items

--- js/script.js
+++ js/script.js
@@ -3,7 +3,6 @@ const fixTechGridHeight = () => {
   const techGrid = document.querySelector('.tech-list');
   const compStyles = window.getComputedStyle(profile);
   const profileHeight = compStyles.getPropertyValue('height');
-  console.log(profileHeight);
   techGrid.style.maxHeight = profileHeight;
 }
 
